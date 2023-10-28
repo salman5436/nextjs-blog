c5720b30ae6486e4e4cfec882b300f20117fc13c
Salman Malik <salman5436@gmail.com>
2023-10-28 14:33:54 -0500

    intial setup of nextjs app with next-auth and google provider login options

---
+++ app/api/auth/hello/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../../api/auth/[...nextAuth]/route'
+
+//protecting our API endpoint:
+//This function below opens the server to an API endpoint (for the sake of testing)
+
+export async function GET() {
+    //This is running on the server, thus the function below
+
+    //we are checking the session
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+        return NextResponse.json({ message: 'You are not logged in'})
+    }
+
+    return NextResponse.json({ name: session.user.name})
+}
